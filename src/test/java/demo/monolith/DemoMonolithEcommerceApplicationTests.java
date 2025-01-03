6e95c9f4a03532fd90b35f9f6f156338b3e28f96
bruderj15 <julian.bruder@outlook.com>
2025-01-03 11:36:37 +0100

    init

---
+++ src/test/java/demo/monolith/DemoMonolithEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMonolithEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
